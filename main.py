860665eda1b42b8878b07d64d59e3a254691a045
Nehanayar <nehanayar27@gmail.com>
2026-06-20 00:59:11 +0530

    Update main.py

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ try:
     from PIL import Image as PILImage
     import pytesseract
 
-    pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
+
     OCR_AVAILABLE = True
 
 except ImportError:
